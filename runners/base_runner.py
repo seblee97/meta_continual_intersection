5da9f02a80c8b1d42c8adb58758c8455e657ff04
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-04-09 10:13:23 +0100

    initial commit of runners module

---
+++ runners/base_runner.py
@@ -0,0 +1,124 @@
+import abc
+
+import constants
+import jax
+import jax.numpy as jnp
+from experiments import maml_config
+from model import maml
+from utils import data_logger, experiment_logger
+
+
+class BaseRunner(abc.ABC):
+    def __init__(self, configuration: maml_config.MAMLConfig):
+
+        self._key = jax.random.PRNGKey(configuration.seed)
+
+        self._task_distribution = self._setup_task_distribution(
+            configuration=configuration
+        )
+        self._model = self._setup_model(configuration=configuration)
+
+        if configuration.log_to_df:
+            self._data_logger = data_logger.DataLogger(configuration=configuration)
+        self._logger = experiment_logger.get_logger(
+            experiment_path=configuration.experiment_path, name=__name__
+        )
+        self._num_epochs = configuration.num_epochs
+        self._num_tasks = configuration.num_tasks
+        self._batch_size = configuration.batch_size
+
+        self._num_evaluations = configuration.num_evaluations
+        self._num_examples = configuration.num_examples
+        self._num_adaptation_steps = configuration.num_adaptation_steps
+        self._plot_evaluations = configuration.plot_evaluations
+
+        self._print_frequency = configuration.print_frequency
+        self._log_to_df = configuration.log_to_df
+        self._checkpoint_frequency = configuration.checkpoint_frequency
+        self._experiment_path = configuration.experiment_path
+
+    @abc.abstractmethod
+    def _setup_task_distribution(self, configuration: maml_config.MAMLConfig):
+        pass
+
+    def _setup_model(self, configuration: maml_config.MAMLConfig):
+        return maml.MAML(
+            key=self._key,
+            inner_optimiser_type=configuration.inner_optimiser_type,
+            outer_optimiser_type=configuration.outer_optimiser_type,
+            inner_lr=configuration.inner_lr,
+            outer_lr=configuration.outer_lr,
+            task_distribution=self._task_distribution,
+            input_dimension=configuration.input_dim,
+            network_specification=configuration.network_specification,
+        )
+
+    def _get_data_batch_from_tasks(self, tasks, batch_size: int):
+        batch_x_inner = []
+        batch_y_inner = []
+        batch_x_outer = []
+        batch_y_outer = []
+
+        for task in tasks:
+
+            x_inner, y_inner = task.sample_data(
+                key=self._key, num_datapoints=batch_size
+            )
+            x_outer, y_outer = task.sample_data(
+                key=self._key, num_datapoints=batch_size
+            )
+
+            batch_x_inner.append(x_inner)
+            batch_y_inner.append(y_inner)
+            batch_x_outer.append(x_outer)
+            batch_y_outer.append(y_outer)
+
+        return (
+            jnp.stack(batch_x_inner),
+            jnp.stack(batch_y_inner),
+            jnp.stack(batch_x_outer),
+            jnp.stack(batch_y_outer),
+        )
+
+    def train(self):
+
+        for i in range(self._num_epochs):
+
+            task_sample = self._task_distribution.sample(
+                key=self._key, num_tasks=self._num_tasks
+            )
+
+            (
+                batch_x_inner,
+                batch_y_inner,
+                batch_x_outer,
+                batch_y_outer,
+            ) = self._get_data_batch_from_tasks(
+                tasks=task_sample, batch_size=self._batch_size
+            )
+
+            outer_optimiser_state, meta_loss = self._model.step(
+                epoch=i,
+                outer_optimiser_state=self._model.outer_optimiser_state,
+                inner_x=batch_x_inner,
+                inner_y=batch_y_inner,
+                outer_x=batch_x_outer,
+                outer_y=batch_y_outer,
+            )
+
+            self._model.outer_optimiser_state = outer_optimiser_state
+
+            if self._log_to_df:
+                self._data_logger.write_scalar(
+                    tag=constants.META_LOSS, step=i, scalar=meta_loss
+                )
+
+            if i % self._print_frequency == 0:
+                self._logger.info(f"{i}: {meta_loss}")
+
+            if i % self._checkpoint_frequency == 0 and i != 0:
+                self._data_logger.checkpoint()
+
+    @abc.abstractmethod
+    def test(self):
+        pass
---
+++ runners/sinusoidal_runner.py
@@ -0,0 +1,65 @@
+import os
+
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+from experiments import maml_config
+from runners import base_runner
+from task_distributions import sinusoidal_task_distribution
+
+
+class SinusoidalRunner(base_runner.BaseRunner):
+    def __init__(self, configuration: maml_config.MAMLConfig):
+
+        self._x_min = configuration.x_min
+        self._x_max = configuration.x_max
+
+        self._amplitude_min = configuration.amplitude_min
+        self._amplitude_max = configuration.amplitude_max
+
+        self._phase_min = configuration.phase_min
+        self._phase_max = configuration.phase_max
+
+        super().__init__(configuration=configuration)
+
+    def _setup_task_distribution(self, configuration: maml_config.MAMLConfig):
+
+        task_distribution = sinusoidal_task_distribution.SinusoidalTaskDistribution(
+            x_range=(self._x_min, self._x_max),
+            amplitude_range=(self._amplitude_min, self._amplitude_max),
+            phase_range=(self._phase_min, self._phase_max),
+        )
+
+        return task_distribution
+
+    def test(self):
+        evaluation_tasks = self._task_distribution.sample(
+            key=self._key, num_tasks=self._num_evaluations
+        )
+
+        trained_parameters = self._model.outer_get_parameters(
+            self._model.outer_optimiser_state
+        )
+
+        for i, task in enumerate(evaluation_tasks):
+            x, y = task.sample_data(key=self._key, num_datapoints=self._num_examples)
+            adapted_parameters = self._model.fine_tune(
+                trained_parameters, x, y, self._num_adaptation_steps
+            )
+
+            if self._plot_evaluations:
+                plot_path = os.path.join(self._experiment_path, f"{i}_test.pdf")
+                self._plot_evaluation(x, y, task, adapted_parameters, plot_path)
+
+    def _plot_evaluation(self, x, y, task, adapted_parameters, save_name):
+        fig = plt.figure()
+        plt.scatter(x, y)
+
+        x_range = jnp.linspace(self._x_min, self._x_max, 100).reshape(-1, 1)
+
+        plt.plot(x_range, task(x_range), label="ground truth")
+        for i, parameters in enumerate(adapted_parameters):
+            regression = self._model.network_forward(parameters, x_range)
+            plt.plot(x_range, regression, label=f"{i} tuning")
+
+        plt.legend()
+        fig.savefig(save_name)
