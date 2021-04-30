f940aa1ed5343e6def977c09a2579d94ab169ff9
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-04-30 14:09:09 +0100

    call method

--- task_distributions/two_layer_teacher_task.py
+++ task_distributions/two_layer_teacher_task.py
@@ -1,61 +1,28 @@
-import base_task
+from typing import Callable, List, Tuple
+
 import jax
 import jax.numpy as jnp
+from task_distributions import base_task
 
 
 class TwoLayerTeacherTask(base_task.BaseTask):
     def __init__(
         self,
         input_dimension: int,
-        hidden_dimension: int,
-        output_dimension: int,
-        activation: str,
+        forward_call: Callable,
+        parameters: List[Tuple[jnp.ndarray]],
     ):
 
-        self._network_forward, self._network_parameters = self._setup_network(
-            input_dimension=input_dimension,
-            hidden_dimension=hidden_dimension,
-            output_dimension=output_dimension,
-            activation=activation,
-        )
+        self._input_dimension = input_dimension
+        self._forward = forward_call
+        self._parameters = parameters
 
         super().__init__()
 
-    def _setup_network(
-        self,
-        input_dimension: int,
-        hidden_dimension: int,
-        output_dimension: int,
-        activation: str,
-    ):
-        layers = []
-
-        layer_1 = stax.Dense(hidden_dimension)
-        layer_2 = stax.Dense(output_dimension)
-
-        if activation == constants.RELU:
-            layer_2_pre_activation = stax.Relu
-        else:
-            raise ValueError("ReLU activation")
-
-        layers.append(layer_1)
-        layers.append(layer_2_pre_activation)
-        layers.append(layer_2)
-
-        init, forward = stax.serial(*layers)
-        _, params = init(self._key, (-1, input_dimension))
-
-        return forward, params
-
     def sample_data(self, key, num_datapoints):
-        x = jax.random.uniform(
-            key=key,
-            shape=(num_datapoints, 1),
-            minval=self._x_range[0],
-            maxval=self._x_range[1],
-        )
-        y = self._amplitude * jnp.sin(x + self._phase)
+        x = jax.random.normal(key=key, shape=(num_datapoints, self._input_dimension))
+        y = self._forward(self._parameters, x)
         return x, y
 
     def __call__(self, x):
-        return self._network_forward(x)
+        return self._forward(x)
